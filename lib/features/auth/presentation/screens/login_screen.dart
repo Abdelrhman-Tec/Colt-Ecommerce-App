a0f73999fdf88865aa10c5e40911d5fef55c1504
Abdelrhman Nada <abdelrhman.nada@gmail.com>
2025-10-23 18:56:42 +0300

    Refactor app structure and improve login screen UI
    - Removed unused imports in app.dart and simplified theme setup.
    - Updated theme mode to light in ColtEcommerceApp.
    - Added AppStrings import to login_screen.dart for better localization.
    - Enhanced login screen UI by setting background color and updating padding.
    - Added sign-in text with improved styling in the login screen.

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:colt_ecommerce_app/core/constants/app_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -12,12 +13,20 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
-            padding: EdgeInsets.only(top: 35.h, right: 27, left: 27),
-            child: Column(children: [
-  
+            padding: EdgeInsets.only(top: 35.h, right: 27.w, left: 27.w),
+            child: Column(
+              children: [
+                Text(
+                  AppStrings.signIn,
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30.sp,
+                  ),
+                ),
               ],
             ),
           ),
