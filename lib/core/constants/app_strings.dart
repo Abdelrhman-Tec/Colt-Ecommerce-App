54be85945e3c655c0a23378b6f798568365f9718
Abdelrhman Nada <abdelrhman.nada@gmail.com>
2025-10-23 18:41:57 +0300

    feat: Update localization strings and clean up package dependencies

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,181 @@
+class AppStrings {
+  // Authentication & Account
+  static const String signIn = 'Sign in';
+  static const String createAccount = 'Create Account';
+  static const String forgotPassword = 'Forgot Password';
+  static const String tellUsAboutYourself = 'Tell us About yourself';
+  static const String emailAddress = 'Email Address';
+  static const String firstname = 'Firstname';
+  static const String enterEmailAddress = 'Enter Email address';
+  static const String whoDoYouShopFor = 'Who do you shop for ?';
+  static const String password = 'Password';
+  static const String lastname = 'Lastname';
+  static const String continueText = 'Continue';
+  static const String men = 'Men';
+  static const String women = 'Women';
+  static const String forgotPasswordReset = 'Forgot Password ? Reset';
+  static const String howOldAreYou = 'How Old are you ?';
+  static const String weSentYouEmail =
+      'We Sent you an Email to reset your password.';
+  static const String ageRange = 'Age Range';
+  static const String dontHaveAccount = 'Dont have an Account ? Create One';
+  static const String returnToLogin = 'Return to Login';
+  static const String continueWithApple = 'Continue With Apple';
+  static const String continueWithGoogle = 'Continue With Google';
+  static const String continueWithFacebook = 'Continue With Facebook';
+  static const String finish = 'Finish';
+
+  // Navigation & Search
+  static const String search = 'Search';
+  static const String notifications = 'Notifications';
+  static const String orders = 'Orders';
+  static const String cart = 'Cart';
+  static const String checkout = 'Checkout';
+  static const String wishlist = 'Wishlist';
+
+  // Categories
+  static const String shopByCategories = 'Shop by Categories';
+  static const String seeAll = 'See All';
+  static const String hoodies = 'Hoodies';
+  static const String accessories = 'Accessories';
+  static const String shorts = 'Shorts';
+  static const String shoes = 'Shoes';
+  static const String bags = 'Bags';
+  static const String topSelling = 'Top Selling';
+  static const String newIn = 'New In';
+  static const String exploreCategories = 'Explore Categories';
+
+  // Product Names
+  static const String mensFleecePulloverHoodie =
+      'Men\'s Fleece Pullover Hoodie';
+  static const String fleecePulloverSkateHoodie =
+      'Fleece Pullover Skate Hoodie';
+  static const String carlMaxCardigan = 'Carl max Cardigan';
+  static const String mensHarringtonJacket = 'Men\'s Harrington Jacket';
+  static const String maxCirroMensSlides = 'Max Cirro Men\'s Slides';
+  static const String mensCoachesJacket = 'Men\'s Coaches Jacket';
+  static const String fleeceSkateHoodie = 'Fleece Skate Hoodie';
+  static const String mensIceDyePulloverHoodie =
+      'Men\'s Ice-Dye Pullover Hoodie';
+  static const String nikeUnscripted = 'Nike Unscripted';
+  static const String nikeSB = 'Nike SB';
+  static const String nikeWindrunner = 'Nike Windrunner';
+  static const String mensMonogramHoodie = 'Men\'s Monogram Hoodie';
+  static const String mensPulloverBasketballHoodie =
+      'Men\'s Pullover Basketball Hoodie';
+  static const String clubFleeceMensJacket = 'Club Fleece Mens Jacket';
+  static const String skateJacket = 'Skate Jacket';
+  static const String thermaFitPufferJacket = 'Therma Fit Puffer Jacket';
+  static const String mensWorkwearJacket = 'Men\'s Workwear Jacket';
+  static const String nikeFuelPack = 'Nike Fuel Pack';
+  static const String nikeShowXRush = 'Nike Show X Rush';
+  static const String mensTShirt = 'Men\'s T-Shirt';
+  static const String mensSkateTShirt = 'Men\'s Skate T-Shirt';
+
+  // Order & Notification
+  static const String order = 'Order';
+  static const String orderConfirmed = 'Order Confirmed';
+  static const String orderPlaced = 'Order Placed';
+  static const String processing = 'Processing';
+  static const String shipped = 'Shipped';
+  static const String delivered = 'Delivered';
+  static const String returned = 'Returned';
+  static const String canceled = 'Canceled';
+  static const String noNotificationYet = 'No Notification yet';
+  static const String noOrdersYet = 'No Orders yet';
+  static const String orderItems = 'Order Items';
+  static const String orderPlacedSuccessfully = 'Order Placed Successfully';
+  static const String youWillReceiveEmail =
+      'You will recieve an email confirmation';
+  static const String seeOrderDetails = 'See Order details';
+
+  // Filter & Sort
+  static const String sortBy = 'Sort by';
+  static const String gender = 'Gender';
+  static const String deals = 'Deals';
+  static const String price = 'Price';
+  static const String recommended = 'Recommended';
+  static const String onSale = 'On sale';
+  static const String newest = 'Newest';
+  static const String freeShippingEligible = 'Free Shipping Eligible';
+  static const String min = 'Min';
+  static const String max = 'Max';
+  static const String lowestHighestPrice = 'Lowest - Highest Price';
+  static const String highestLowestPrice = 'Highest - Lowest Price';
+  static const String clear = 'Clear';
+  static const String resultsFound = 'Results Found';
+  static const String noMatchingResults =
+      'Sorry, we couldn\'t find any matching result for your Search.';
+
+  // Product Details
+  static const String size = 'Size';
+  static const String color = 'Color';
+  static const String quantity = 'Quantity';
+  static const String addToBag = 'Add to Bag';
+  static const String removeAll = 'Remove All';
+  static const String yourCartIsEmpty = 'Your Cart is Empty';
+  static const String enterCouponCode = 'Enter Coupon Code';
+  static const String subtotal = 'Subtotal';
+  static const String shippingCost = 'Shipping Cost';
+  static const String tax = 'Tax';
+  static const String total = 'Total';
+  static const String placeOrder = 'Place Order';
+
+  // Sizes
+  static const String sizeS = 'S';
+  static const String sizeM = 'M';
+  static const String sizeL = 'L';
+  static const String sizeXL = 'XL';
+  static const String size2XL = '2XL';
+
+  // Colors
+  static const String colorLemon = 'Lemon';
+  static const String colorBlack = 'Black';
+  static const String colorOrange = 'Orange';
+  static const String colorRed = 'Red';
+  static const String colorYellow = 'Yellow';
+  static const String colorBlue = 'Blue';
+
+  // Shipping & Address
+  static const String shippingAddress = 'Shipping Address';
+  static const String addShippingAddress = 'Add Shipping Address';
+  static const String addAddress = 'Add Address';
+  static const String streetAddress = 'Street Address';
+  static const String city = 'City';
+  static const String state = 'State';
+  static const String zipCode = 'Zip Code';
+
+  // Payment
+  static const String paymentMethod = 'Payment Method';
+  static const String addPaymentMethod = 'Add Payment Method';
+  static const String addCard = 'Add Card';
+  static const String cardNumber = 'Card Number';
+  static const String cardholderName = 'Cardholder Name';
+  static const String ccv = 'CCV';
+  static const String exp = 'Exp';
+
+  // User Profile
+  static const String myFavourites = 'My Favourites';
+  static const String myFavorite = 'My Favorite';
+  static const String edit = 'Edit';
+  static const String help = 'Help';
+  static const String support = 'Support';
+  static const String signOut = 'Sign Out';
+  static const String save = 'Save';
+
+  // Product Descriptions
+  static const String productDescription =
+      'Built for life and made to last, this full-zip corduroy jacket is part of our Nike Life collection. The spacious fit gives you plenty of room to layer underneath, while the soft corduroy keeps it casual and timeless.';
+  static const String gucciDescription =
+      'Gucci transcribes its heritage, creativity, and innovation into a plenitude of collections. From staple items to distinctive accessories.';
+
+  // Shipping & Returns Info
+  static const String shippingReturns = 'Shipping & Returns';
+  static const String freeShippingReturns =
+      'Free standard shipping and free 60-day returns';
+
+  // Reviews
+  static const String reviews = 'Reviews';
+  static const String ratings = 'Ratings';
+  static const String daysAgo = 'days ago';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:colt_ecommerce_app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
 
 void main() async {
-  runApp();
+  runApp(const ColtEcommerceApp());
 }
