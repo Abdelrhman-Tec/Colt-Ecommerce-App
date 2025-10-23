2066f77a06847bea427334838b02b26a4ebdbfdc
Abdelrhman Nada <abdelrhman.nada@gmail.com>
2025-10-23 14:42:03 +0300

    feat: Initialize core structure and implement API error handling
    - Added core configuration and app setup files.
    - Implemented API error handling with detailed error messages.
    - Created caching mechanism using SharedPreferences.
    - Developed localization support with English and Arabic languages.
    - Established theming with light and dark modes.
    - Integrated navigation extensions for easier route management.
    - Added regex utilities for input validation.
    - Set up API result handling with success and failure states.
    - Included necessary dependencies for Dio and localization.

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF8B6CFF);
+
+  //Light Theme
+  static const Color lightBackground = Color(0xFFFFFFFF);
+  static const Color lightSurface = Color(0xFFF5F5F5);
+  static const Color lightTextPrimary = Color(0xFF1C1C1E);
+  static const Color lightTextSecondary = Color(0xFF6E6E73);
+
+  // Dark Theme
+  static const Color darkBackground = Color(0xFF0E0E12);
+  static const Color darkSurface = Color(0xFF1C1C1E);
+  static const Color darkTextPrimary = Color(0xFFFFFFFF);
+  static const Color darkTextSecondary = Color(0xFFB0B0B0);
+
+  //Accent
+  static const Color accent = Color(0xFFFFC83A);
+}
