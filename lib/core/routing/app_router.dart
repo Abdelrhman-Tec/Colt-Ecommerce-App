0353514639d2c56fb64be0a4ea92cf47010d61db
Abdelrhman Nada <abdelrhman.nada@gmail.com>
2025-10-24 18:35:49 +0300

    feat: Add Forgot Password and Register screens with animated transitions

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,15 +1,41 @@
 import 'package:colt_ecommerce_app/core/routing/routes.dart';
+import 'package:colt_ecommerce_app/features/auth/presentation/screens/forgot_password_screen.dart';
 import 'package:colt_ecommerce_app/features/auth/presentation/screens/login_screen.dart';
+import 'package:colt_ecommerce_app/features/auth/presentation/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return _animatedRoute(const LoginScreen());
+      case Routes.registerScreen:
+        return _animatedRoute(const RegisterScreen());
+      case Routes.forgotPasswordScreen:
+        return _animatedRoute(const ForgotPasswordScreen());
+
       default:
         return _errorRoute(settings.name);
     }
+  }
+
+  static Route<dynamic> _animatedRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
+
+        var tween = Tween(
+          begin: begin,
+          end: end,
+        ).chain(CurveTween(curve: curve));
+
+        return SlideTransition(position: animation.drive(tween), child: child);
+      },
+      transitionDuration: const Duration(milliseconds: 400),
+    );
   }
 
   static Route<dynamic> _errorRoute(String? name) {
@@ -17,7 +43,7 @@ class AppRouter {
       builder: (_) => Scaffold(
         body: Center(
           child: Text(
-            ' No route defined for "$name"',
+            'No route defined for "$name"',
             style: const TextStyle(fontSize: 18),
           ),
         ),
