9de8476d18f03c740c1dd836c9ac3689551eeaf3
Abdelrhman Nada <abdelrhman.nada@gmail.com>
2025-10-24 15:59:11 +0300

    implement Registration via social media

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:colt_ecommerce_app/core/constants/app_strings.dart';
 import 'package:colt_ecommerce_app/core/helpers/spacing.dart';
-import 'package:colt_ecommerce_app/features/auth/presentation/widgets/custom_text_form_filed.dart';
+import 'package:colt_ecommerce_app/core/theme/app_colors.dart';
+import 'package:colt_ecommerce_app/core/widgets/custom_button.dart';
+import 'package:colt_ecommerce_app/core/widgets/custom_text_form_filed.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -23,13 +25,15 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Padding(
             padding: EdgeInsets.only(top: 80.h, right: 27.w, left: 27.w),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  AppStrings.signIn,
-                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 30.sp,
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    AppStrings.signIn,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 30.sp,
+                    ),
                   ),
                 ),
                 verticalSpace(20),
@@ -51,6 +55,63 @@ class _LoginScreenState extends State<LoginScreen> {
                         ? const Icon(Icons.visibility_off_outlined, size: 24)
                         : const Icon(Icons.visibility_outlined, size: 24),
                   ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      AppStrings.forgotPassword,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                verticalSpace(100),
+                CustomButton(
+                  backgroundColor: AppColors.primary,
+                  text: AppStrings.continueText,
+                  textColor: AppColors.lightBackground,
+                  borderRadius: 50,
+                  onPressed: () {},
+                ),
+                verticalSpace(10),
+                Text.rich(
+                  TextSpan(
+                    text: AppStrings.dontHaveAccount,
+                    children: [TextSpan(text: AppStrings.createOne)],
+                  ),
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodySmall!.copyWith(fontWeight: FontWeight.w500),
+                ),
+                verticalSpace(100),
+                CustomButton(
+                  textColor: Theme.of(context).textTheme.titleLarge!.color!,
+                  iconSize: 25,
+                  icon: Icons.apple,
+                  backgroundColor: Theme.of(
+                    context,
+                  ).colorScheme.primary.withAlpha(30),
+                  borderRadius: 50,
+                  text: "Continue With Apple",
+                  onPressed: () {},
+                ),
+                verticalSpace(10),
+
+                verticalSpace(10),
+                CustomButton(
+                  textColor: Theme.of(context).textTheme.titleLarge!.color!,
+                  iconSize: 20,
+                  icon: Icons.facebook,
+
+                  backgroundColor: Theme.of(
+                    context,
+                  ).colorScheme.primary.withAlpha(30),
+                  borderRadius: 50,
+                  text: "Continue With Facebook",
+                  onPressed: () {},
                 ),
               ],
             ),
