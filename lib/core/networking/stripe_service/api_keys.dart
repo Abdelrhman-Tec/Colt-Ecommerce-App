19135813af8dfdebcdce1bdec570aedc3af53e1b
Abdelrhman Nada <abdelrhman.nada@gmail.com>
2025-10-29 22:09:48 +0300

    remove api keys

--- lib/core/networking/stripe_service/api_keys.dart
+++ lib/core/networking/stripe_service/api_keys.dart
@@ -1,6 +1,4 @@
 abstract class ApiKeys {
-  static const String secretKey =
-      'sk_test_51SC5nhCflShdKtPKAUuNdDBThaeLDH83jmb1oo1MhMUcen6bxmztxw6WB3P7oIuDDAcFvZCxzr2Tr4xbiyDU8Hl300iEenY1vR';
-  static const String publishableKey =
-      'pk_test_51SC5nhCflShdKtPKS6DFnsnvxffhTM8yWMyGI0a2WoPC2Bq9jJPfnsKZ9nj3rReMmmmaHRwWMXyYwycbf5PswwQ200NLcBWjNO';
+  static const String secretKey = '';
+  static const String publishableKey = '';
 }
